1f4e490df0b248475b1ce8863ba2873c67da7de0
3Mydlo3 <mydlo333@interia.pl>
2020-11-13 14:22:29 +0100

    Replace remaining occurences

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT main
-#include "\z\mai\addons\main\script_mod.hpp"
+#include "\z\afmf\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
@@ -11,4 +11,4 @@
     #define DEBUG_SETTINGS DEBUG_SETTINGS_MAIN
 #endif
 
-#include "\z\mai\addons\main\script_macros.hpp"
+#include "\z\afmf\addons\main\script_macros.hpp"
