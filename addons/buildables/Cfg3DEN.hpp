f0937165d12435fda0353142ed48643a257e98b9
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-05-15 04:43:59 +0200

    Add twoPanel 3DEN control

--- addons/buildables/Cfg3DEN.hpp
+++ addons/buildables/Cfg3DEN.hpp
@@ -26,5 +26,91 @@ class Cfg3DEN {
                 };
             };
         };
+
+        class GVAR(twoPanel): ctrlControlsGroupNoScrollbars {
+
+            idc = -1;
+
+            x = QUOTE(0);
+            y = QUOTE(0);
+            w = QUOTE(130 * ATTRIBUTE_W);
+            h = QUOTE((10 + 5 + 5 + 25) * ATTRIBUTE_H);
+
+            attributeLoad = "";
+            attributeSave = "";
+
+            class Controls {
+                #define COL_W 55
+                #define BTN_W ((125 - (COL_W * 2)) / 2)
+
+                class CategoriesTitle: ctrlStatic {
+                    idc = -1;
+
+                    text = "Title";
+                    tooltip = "Tooltip";
+
+                    x = QUOTE(5 * ATTRIBUTE_W);
+                    y = QUOTE(0);
+                    w = QUOTE(125 * ATTRIBUTE_W);
+                    h = QUOTE(5 * ATTRIBUTE_H);
+                };
+
+                class ClassesSearch: ctrlEdit {
+                    idc = 123;
+
+                    x = QUOTE((5 + 2.5) * ATTRIBUTE_W);
+                    y = QUOTE(7 * ATTRIBUTE_H);
+                    w = QUOTE(COL_W * ATTRIBUTE_W);
+                    h = QUOTE(5 * ATTRIBUTE_H);
+
+                    tooltip = "Type to search";
+                };
+                class ClassesTree: ctrlTree {
+                    idc = 100;
+                    idcSearch = 123;
+
+                    onLoad = "\
+                        params ['_tv'];\
+                        _classes = ""getNumber (_x >> 'scope') == 2"" configClasses (configFile >> 'CfgVehicles');\
+                        {\
+                            _tv tvAdd [[], configName _x];\
+                        } forEach _classes;\
+                    ";
+
+                    colorBackground[] = {0,0,0,0.5};
+
+                    x = QUOTE((5 + 2.5) * ATTRIBUTE_W);
+                    y = QUOTE(14 * ATTRIBUTE_H);
+                    w = QUOTE(COL_W * ATTRIBUTE_W);
+                    h = QUOTE(25 * ATTRIBUTE_H);
+                };
+
+                class ButtonAdd: ctrlButton {
+                    text = "+";
+
+                    x = QUOTE( (COL_W + BTN_W / 2 + BTN_W / 3 + 2.5) * ATTRIBUTE_W);
+                    y = QUOTE(20 * ATTRIBUTE_H);
+                    w = QUOTE( BTN_W * ATTRIBUTE_W);
+                    h = QUOTE( BTN_W * ATTRIBUTE_W);
+                };
+                class ButtonRemove: ctrlButton {
+                    text = "-";
+
+                    x = QUOTE( (COL_W + BTN_W / 2 + BTN_W / 3 + 2.5) * ATTRIBUTE_W);
+                    y = QUOTE(30 * ATTRIBUTE_H);
+                    w = QUOTE( BTN_W * ATTRIBUTE_W);
+                    h = QUOTE( BTN_W * ATTRIBUTE_W);
+                };
+
+                class SelectedList: ctrlListbox {
+                    idc = 101;
+
+                    x = QUOTE((70 + 2.5) * ATTRIBUTE_W);
+                    y = QUOTE(14 * ATTRIBUTE_H);
+                    w = QUOTE(COL_W * ATTRIBUTE_W);
+                    h = QUOTE(25 * ATTRIBUTE_H);
+                };
+            };
+        };
     };
 };
