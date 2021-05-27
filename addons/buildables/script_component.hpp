962dd92f56616ffa1fdbb4a3c72cec913695702e
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-05-27 03:01:03 +0200

    Disable debug

--- addons/buildables/script_component.hpp
+++ addons/buildables/script_component.hpp
@@ -4,8 +4,8 @@
 #include "\a3\ui_f\hpp\defineResincl.inc"
 #include "\a3\ui_f\hpp\defineCommonColors.inc"
 
-#define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 
 #include "\z\afmf\addons\main\script_macros.hpp"
 
